196c6f831450f353235c54f1b82255efdbf12afa
gomihitomi <gomihitomi@gmail.com>
2026-01-24 20:10:20 +0900

    [fix]文言微調整

--- src/features/sections/guideline.tsx
+++ src/features/sections/guideline.tsx
@@ -13,22 +13,22 @@ export const Guideline = () => {
     <Section {...sectionItem}>
       <div>
         <Paragraph isFirst>
-          吾味人美は基本的に楽しんで欲しいというスタンスで活動をしています。
-          その為、下記ガイドラインを守って頂ければ好きなように二次創作・二次使用をして頂いて構いません。
+          吾味人美は、基本的に皆様に楽しんで欲しいというスタンスで活動をしています。
+          以下のガイドラインをお守りいただける場合に限り、吾味人美に関する二次創作および二次利用を自由に行っていただいて問題ありません。
         </Paragraph>
         <Paragraph>
-          もしも、悩む様なケースがありましたら、別途お問い合わせ頂けますと幸いです。
+          もしも、判断に迷うケースがありましたら、個別にお問い合わせいただけますと幸いです。
         </Paragraph>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-4">
           <div>
             <span className="font-bold text-xl">◯ OK</span>
             <ul>
               <li>
-                <span className="font-bold">吾味人美の二次創作をすること</span>
+                <span className="font-bold">吾味人美の二次創作を行うこと</span>
               </li>
               <ol className="list-disc list-outside pl-5">
                 <li>
-                  吾味人美を対象に制作したファンアート・切り抜き動画などすべての二次創作が対象となります。
+                  吾味人美を題材として制作されたファンアート・切り抜き動画など、すべての二次創作が対象となります。
                 </li>
                 <li>
                   制作・投稿時の詳細については
@@ -39,11 +39,11 @@ export const Guideline = () => {
                 </li>
               </ol>
               <li>
-                <b>吾味人美のコンテンツを二次使用すること</b>
+                <b>吾味人美のコンテンツを二次利用すること</b>
               </li>
               <ol className="list-disc list-outside pl-5">
                 <li>
-                  吾味人美の制作した絵・音楽・動画などすべての制作物が対象となります。
+                  吾味人美が制作したイラスト、音楽、動画などすべての制作物が対象となります。
                 </li>
                 <li>
                   ただし、吾味人美以外の著作物が含まれる二次創作・ファンアートなどは対象となりません。
@@ -55,7 +55,7 @@ export const Guideline = () => {
             <span className="font-bold text-xl">✖ NG</span>
             <ul>
               <li>
-                <b>下記の用途で利用すること</b>
+                <b>以下の目的、内容での二次創作、二次利用を行うこと</b>
               </li>
               <ol className="list-disc list-outside pl-5">
                 <li>営利目的（商用）での利用</li>
